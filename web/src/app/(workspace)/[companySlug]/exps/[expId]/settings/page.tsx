331c8ac9e3da1d9bcf8da5282fe26c8635c4047a
Igor Vileikis <festys.rpo@gmail.com>
2025-12-03 20:52:33 +0200

    update layout of experiences

--- web/src/app/(workspace)/[companySlug]/exps/[expId]/settings/page.tsx
+++ web/src/app/(workspace)/[companySlug]/exps/[expId]/settings/page.tsx
@@ -35,6 +35,7 @@ export default async function SettingsPage({ params }: SettingsPageProps) {
 
   return (
     <ExperienceSettingsClient
+      companySlug={companySlug}
       companyId={company.id}
       initialExperience={experience}
     />
