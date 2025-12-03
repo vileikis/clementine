ec965ab9261164dd67ad6734cd99fd6457c1958d
Igor Vileikis <festys.rpo@gmail.com>
2025-12-03 20:48:59 +0200

    delete journey module

--- web/src/features/journeys/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useSteps } from "./useSteps";
-export { useStepMutations } from "./useStepMutations";
-export { useSelectedStep } from "./useSelectedStep";
-export { useEventAiPresets, useEventExperiences } from "./useEventAiPresets";
-export { useKeyboardShortcuts } from "./useKeyboardShortcuts";
