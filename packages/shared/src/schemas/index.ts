b11e0f347d011672a7648fe1a3468896e5bac55d
Igor Vileikis <festys.rpo@gmail.com>
2025-12-15 15:14:09 +0200

    simplify tsconfig and incremeant deps

--- packages/shared/src/schemas/index.ts
+++ packages/shared/src/schemas/index.ts
@@ -1,12 +1,1 @@
-export {
-  inputAssetSchema,
-  type InputAsset,
-  processingErrorSchema,
-  type ProcessingError,
-  processingStateSchema,
-  type ProcessingState,
-  sessionOutputsSchema,
-  type SessionOutputs,
-  sessionProcessingSchema,
-  type SessionProcessing,
-} from "./session.schemas"
+export * from "./session.schemas"
