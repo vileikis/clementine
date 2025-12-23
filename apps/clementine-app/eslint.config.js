44e6bb130b96b633ddf7a8493980602d51a41fd6
Igor Vileikis <festys.rpo@gmail.com>
2025-12-23 16:28:28 +0200

    proper eslint config

--- apps/clementine-app/eslint.config.js
+++ apps/clementine-app/eslint.config.js
@@ -1,5 +1,26 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import tseslint from 'typescript-eslint'
 
-export default [...tanstackConfig]
+export default [
+  {
+    ignores: [
+      '.output/**',
+      'src/examples/**',
+      'src/lib/**',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  },
+  ...tanstackConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    },
+  },
+]
--- apps/clementine-app/src/routes/share/index.tsx
+++ apps/clementine-app/src/routes/share/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 import { getSessions } from '../../data/sessions'
 
 export const Route = createFileRoute('/share/')({
