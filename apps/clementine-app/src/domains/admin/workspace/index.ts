521b012feb151960f7ff020e5fcbde55db74f95f
Igor Vileikis <festys.rpo@gmail.com>
2025-12-28 19:47:19 +0200

    clean up

--- apps/clementine-app/src/domains/admin/workspace/index.ts
+++ apps/clementine-app/src/domains/admin/workspace/index.ts
@@ -14,8 +14,8 @@ export { useDeleteWorkspace } from './hooks/useDeleteWorkspace'
 export { WorkspaceList } from './components/WorkspaceList'
 export { WorkspaceListEmpty } from './components/WorkspaceListEmpty'
 export { WorkspaceListItem } from './components/WorkspaceListItem'
-export { CreateWorkspaceSheet } from './components/CreateWorkspaceSheet'
 export { DeleteWorkspaceDialog } from './components/DeleteWorkspaceDialog'
 
 // Containers (page components)
 export { WorkspacesPage } from './containers/WorkspacesPage'
+export { CreateWorkspacePage } from './containers/CreateWorkspacePage'
