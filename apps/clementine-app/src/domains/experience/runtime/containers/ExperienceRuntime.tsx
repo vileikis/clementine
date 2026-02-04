baa0cace2e61a344a4eea273d41292aa8523fac4
Igor Vileikis <festys.rpo@gmail.com>
2026-02-04 21:42:39 +0200

    scrolabble to step layout

--- apps/clementine-app/src/domains/experience/runtime/containers/ExperienceRuntime.tsx
+++ apps/clementine-app/src/domains/experience/runtime/containers/ExperienceRuntime.tsx
@@ -267,7 +267,7 @@ export function ExperienceRuntime({
           onHomeClick={onHomeClick}
         />
       )}
-      <div className="flex-1 min-h-0 overflow-y-auto">{children}</div>
+      <div className="flex-1 min-h-0">{children}</div>
     </div>
   )
 }
